6dd77950ef07a9e12105fcf72937c3f1c7f972cf
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2019-02-14 15:58:17 +0100

    generates frontpage from cli

--- pust0/report_generator.py
+++ pust0/report_generator.py
@@ -1,9 +1,23 @@
 import argparse
 
+def get_coursename() -> str:
+    """ Acquire course name """
+    course_name = input("Course name: ")
+    return course_name 
+
+def get_projectname() -> str:
+    """ Acquire project name """
+    project_name = input("Project name: ")
+    return project_name
+
+def get_teacher() -> str:
+    """ Acquire teacher name """
+    teacher_name = input("Teacher name: ")
+    return teacher_name
+
 def get_authors() -> str:
-    """
-        Acquire authors name and create string for tex file
-    """
+    """ Acquire authors name and create string for tex file """
+
     authors = []
     while True:
         author = input("Please provide project author name (empty line to finish): ")
@@ -28,21 +42,25 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-fp", "--frontpage", help="generate frontpage", action="store_true")
     parser.add_argument("-ch", "--chapter", help="generate new chapter", action="store_true")
+    parser.add_argument("-uf", "--use-conf-file", help="use configuration file", action="store_true")
     parser.add_argument("path", help="path to folder where to create new file")
     args = parser.parse_args()
 
     if args.frontpage:
         
+        course_name = get_coursename()
+        project_name = get_projectname()
         author_str = get_authors()
+        teacher_name = get_teacher()
 
         with open("./resources/main.tex") as file:
             tex_template = file.read() 
 
         print("Generating frontpage...")
+        tex_template = tex_template.replace("__COURSE_NAME__", course_name)
+        tex_template = tex_template.replace("__PROJECT_NAME__", project_name)
         tex_template = tex_template.replace("__AUTHORS__", author_str)
-        tex_template = tex_template.replace("__PROJECT_NAME__", "Projekt 1")
-        tex_template = tex_template.replace("__COURSE_NAME__", "Projektowanie układów sterowania")
-        tex_template = tex_template.replace("__TEACHER__", "dr inż. Patryk Chaber")
+        tex_template = tex_template.replace("__TEACHER__", teacher_name)
         with open(args.path + "/main.tex", mode="w") as file:
             file.write(tex_template)
         exit(0)
