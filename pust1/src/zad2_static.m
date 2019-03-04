43f79542434b52401bfa883787ed3cc0e9b728bb
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2019-03-04 16:08:44 +0100

    #7 eval static characteristic of object

---
+++ pust1/src/zad2_static.m
@@ -0,0 +1,29 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Projektowanie układów sterowania
+%          Projekt 1, zadanie 1
+%
+%   Zebranie kilku odpowiedzi skokowych 
+%   obiektu                               
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% wczytanie funkcji symulującej obiekt i generacja wektorów bazowych
+addpath ../
+STATIC_CHAR_DENSITY = 100;
+SIM_LEN = 200;
+inputs = (linspace(0.3, 0.7, STATIC_CHAR_DENSITY))';
+saturated_outputs = zeros(STATIC_CHAR_DENSITY, 1);
+
+%% symulacja procesu i zebranie ostatniej wartości
+for i=1:length(inputs)
+    sim_input = inputs(i)*ones(SIM_LEN,1);
+    sim_output = zeros(SIM_LEN,1);
+    for k=12:SIM_LEN
+        sim_output(k) = symulacja_obiektu1Y(sim_input(k-10), sim_input(k-11), sim_output(k-1), sim_output(k-2));
+    end
+    saturated_outputs(i) = sim_output(SIM_LEN);
+end
+
+%% zapisanie charakterystki statycznej
+plot(inputs, saturated_outputs);
+static_char = [inputs saturated_outputs];
+dlmwrite("../data/zad2_static_char.csv", static_char, '\t');
