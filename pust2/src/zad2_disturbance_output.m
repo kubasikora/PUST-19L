ffa4d135032494a646147fa48bbecbb3592eb3f8
wojtasRobert <283234@pw.edu.pl>
2019-03-22 12:20:43 +0100

    #28 #29 popraweczki

--- pust2/src/zad2_disturbance_output.m
+++ pust2/src/zad2_disturbance_output.m
@@ -25,16 +25,16 @@ grid minor;
 for i=1:length(deltas)
     sim_disturbance = base_disturbance + deltas(i);
     sim_output = base_output;
-    for k=12:sim_len
+    for k=8:sim_len
         sim_output(k) = symulacja_obiektu1y(input(k-6),input(k-7),sim_disturbance(k-2),sim_disturbance(k-3),sim_output(k-1),sim_output(k-2));
     end
     plot(time, sim_output);
-    input_ts = [time-1 sim_input];
+    disturbance_ts = [time-1 sim_disturbance];
     output_ts = [time-1 sim_output];
-    dlmwrite("../data/zad2_disturbance_disturbance" + num2str(deltas(i)) + ".csv", input_ts, '\t');
-    dlmwrite("../data/zad2_disturbance_output" + num2str(deltas(i)) + ".csv", output_ts, '\t');
+    dlmwrite("../data/zad2/zad2_disturbance_disturbance" + num2str(deltas(i)) + ".csv", disturbance_ts, '\t');
+    dlmwrite("../data/zad2/zad2_disturbance_output" + num2str(deltas(i)) + ".csv", output_ts, '\t');
     merged_outputs(i+1,:) = sim_output';
 end
 hold off
 %% zapis wszystkich wyjsc do zbiorczego rysunku
-dlmwrite("../data/zad2_disturbance_outputs_merged.csv", merged_outputs, '\t');
+dlmwrite("../data/zad2/zad2_disturbance_outputs_merged.csv", merged_outputs, '\t');
--- pust2/src/zad2_input_output.m
+++ pust2/src/zad2_input_output.m
@@ -1,4 +1,4 @@
-%% wczytanie funkcji symulującej obiekt i generacja wektorów bazowych w punkcie pracy
+%% wczytanie funkcji symulujacej obiekt i generacja wektorow bazowych w punkcie pracy
 addpath ../
 
 sim_len = 200;
@@ -25,16 +25,16 @@ grid minor;
 for i=1:length(deltas)
     sim_input = base_input + deltas(i);
     sim_output = base_output;
-    for k=12:sim_len
+    for k=8:sim_len
         sim_output(k) = symulacja_obiektu1y(sim_input(k-6), sim_input(k-7),disturbance(k-2), disturbance(k-3), sim_output(k-1),sim_output(k-2));
     end
     plot(time, sim_output);
     input_ts = [time-1 sim_input];
     output_ts = [time-1 sim_output];
-    dlmwrite("../data/zad2_input_input" + num2str(deltas(i)) + ".csv", input_ts, '\t');
-    dlmwrite("../data/zad2_input_output" + num2str(deltas(i)) + ".csv", output_ts, '\t');
+    dlmwrite("../data/zad2/zad2_input_input" + num2str(deltas(i)) + ".csv", input_ts, '\t');
+    dlmwrite("../data/zad2/zad2_input_output" + num2str(deltas(i)) + ".csv", output_ts, '\t');
     merged_outputs(i+1,:) = sim_output';
 end
 hold off
 %% zapis wszystkich wyjsc do zbiorczego rysunku
-dlmwrite("../data/zad2_input_outputs_merged.csv", merged_outputs, '\t');
+dlmwrite("../data/zad2/zad2_input_outputs_merged.csv", merged_outputs, '\t');
