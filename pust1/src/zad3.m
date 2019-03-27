a6fbe0c808caf4c06a37f4b018ebc0696d6ea66b
unknown <doli33@op.pl>
2019-03-28 00:17:17 +0100

    Skonczoen wykresy do zad 3 lab

--- pust1/src/zad3.m
+++ pust1/src/zad3.m
@@ -9,7 +9,7 @@
 %% wczytanie funkcji symulującej i inicjalizacja symulacji
 addpath ../
 Upp = 0.5;
-Ypp = 4;
+Ypp = 32;
 SIM_LEN = 200;
 time = (1:SIM_LEN)';
 deltaU = 0.1;
@@ -30,7 +30,7 @@ output = output / deltaU;
 %% zapisanie znormalizowanej odpowiedzi skokowej
 norm_resp = [time output];
 plot(time, output);
-dlmwrite("../data/zad3_norm_odp.csv", norm_resp, '\t');
+dlmwrite('../data/zad3_norm_odp_____________.csv', norm_resp, '\t');
 
 
 %% obcięcie odpowiedzi skokowej
@@ -48,4 +48,4 @@ cut_time_vect = transpose(1:(cut_time));
 cut_response = output(1:cut_time);
 plot(cut_time_vect, cut_response);
 cut_resp = [cut_time_vect cut_response];
-dlmwrite("../data/zad3_cut_norm_odp.csv", cut_resp, '\t');
+dlmwrite('../data/zad3_cut_norm_odp.csv', cut_resp, '\t');
--- pust2/zad3_lab_mat2csv.m
+++ pust2/zad3_lab_mat2csv.m
@@ -1,4 +1,4 @@
 %szybki skrypt do zad3
 s1 = step(tf11);
 s1_ts = [(1:length(s1))' s1];
-dlmwrite(strcat('data/lab/zad3/apro_11_s.csv'), s1_ts, '\t');
+dlmwrite(strcat('data/lab/zad3/apro_3_sz.csv'), s1_ts, '\t');
