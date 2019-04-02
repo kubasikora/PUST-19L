d366931ace1970fcb110e419fde846dbee2b151f
unknown <doli33@op.pl>
2019-04-03 00:53:39 +0200

    Skończone zad 3 laba

--- pust2/zad3_lab_mat2csv.m
+++ pust2/zad3_lab_mat2csv.m
@@ -1,4 +1,4 @@
 %szybki skrypt do zad3
-s1 = step(tf11);
+s1 = step(tf3_do_3)%plot(norm_do_wykresu); % tu powinien byæ step
 s1_ts = [(1:length(s1))' s1];
 dlmwrite(strcat('data/lab/zad3/apro_3_sz.csv'), s1_ts, '\t');
