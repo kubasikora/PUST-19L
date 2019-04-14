055c631cd1e41f6d7843ef18a410625cf433db06
wojtasRobert <283234@pw.edu.pl>
2019-04-14 21:32:14 +0200

    #76 check that fuzzy dmc out

--- pust3/src/simulate_steps.m
+++ pust3/src/simulate_steps.m
@@ -1,9 +1,13 @@
 function step = simulate_steps(Upp, Ypp)
     SIM_LEN = 500;
-    du = 0.1;
+    du = 0.2;
+    
     u = (du + Upp) * ones(SIM_LEN,1); % skok sterowania
+    
     u(1:6) = Upp;
+    
     y = Ypp * ones(SIM_LEN,1);
+    
     for k = 7:SIM_LEN
        y(k) = symulacja_obiektu1y(u(k-5), u(k-6), y(k-1), y(k-2));
     end
