78cb951d5f27ad66b6e64e7e501be24f85d38788
wojtasRobert <283234@pw.edu.pl>
2019-04-15 01:46:19 +0200

    bad fdmc

--- pust3/src/simulate_steps.m
+++ pust3/src/simulate_steps.m
@@ -1,6 +1,6 @@
 function step = simulate_steps(Upp, Ypp)
     SIM_LEN = 500;
-    du = 0.2;
+    du = 0.06;
     
     u = (du + Upp) * ones(SIM_LEN,1); % skok sterowania
     
@@ -10,10 +10,9 @@ function step = simulate_steps(Upp, Ypp)
     
     for k = 7:SIM_LEN
        y(k) = symulacja_obiektu1y(u(k-5), u(k-6), y(k-1), y(k-2));
-    end
-    
-    
+    end   
     step = y - Ypp;
     step = step / du;
+    figure(10)
     plot(step);
 end
