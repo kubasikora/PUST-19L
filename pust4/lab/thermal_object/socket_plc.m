6c3e728078e0063a3c608dd3926dd7394b86ca25
Jakub Sikora <jakubsikora@adres.pl>
2019-06-03 15:05:44 +0200

    koneic labek w pizdu:

--- pust4/lab/thermal_object/socket_plc.m
+++ pust4/lab/thermal_object/socket_plc.m
@@ -11,32 +11,30 @@ t.OutputBufferSize = 3000;
 t.InputBufferSize = 3000;
  
 fopen(t);
-u1 = [];
-y1 = [];
-u2 = [];
-y2 = [];
+U_x = [];
+U_y = [];
+Y_x = [];
+Y_y = [];
 stpt1 = [];
 stpt2 = [];
 
 figure(1);
-while (length(y1) < 1000)
+while (length(Y_x) < 1000)
     if (t.BytesAvailable ~= 0)
         temp = fscanf(t);
         disp(temp);
         eval(temp);
-        u1 = [u1; U1/10; U1/10; U1/10; U1/10;];
-        y1 = [y1; Y1/100; Y1/100; Y1/100; Y1/100];
-        u2 = [u2; U2/10; U2/10; U2/10; U2/10;];
-        y2 = [y2; Y2/100; Y2/100; Y2/100; Y2/100];
+        U_x = [U_x; u_x];
+        U_y = [U_y; u_y];
+        Y_x = [Y_x; y_x];
+        Y_y = [Y_y; y_y];
         stpt1 = [stpt1; STPT1];
         stpt2 = [stpt2; STPT2];
         
-        subplot(4,1,1); plot(y1); title('Wyjœcie 1'); xlabel('iteracja'); 
-        subplot(4,1,2); plot(y2); title('Wyjœcie 2'); xlabel('iteracja'); 
-        
-   
-        subplot(4,1,3); stairs(u1); title('Sterowanie 1'); xlabel('iteracja');
-        subplot(4,1,4); stairs(u2); title('Sterowanie 2'); xlabel('iteracja');
+        subplot(4,1,1); plot(Y_x); title('Wyjœcie 1'); xlabel('iteracja');  
+        subplot(4,1,2); plot(Y_y); title('Wyjœcie 2'); xlabel('iteracja'); 
+        subplot(4,1,3); stairs(U_x); title('Sterowanie 1'); xlabel('iteracja');
+        subplot(4,1,4); stairs(U_y); title('Sterowanie 2'); xlabel('iteracja');
         drawnow
     end
     pause(0.05);
